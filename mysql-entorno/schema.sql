1646e3aaf0c5a1d4719609438a6c72803427098a
Palomo-33 <pablomarinortega37@gmail.com>
2026-03-19 11:32:48 +0100

    mas ajustes de han hecho al schema y se han empezado a realizar los INSERTS (marcas hecho)

--- mysql-entorno/schema.sql
+++ mysql-entorno/schema.sql
@@ -1,34 +1,42 @@
 CREATE DATABASE IF NOT EXISTS tienda_coches;
-USE tienda_coches
+USE tienda_coches;
 
-CREATE TABLE marca(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nombre TEXT NOT NULL UNIQUE
-)
+CREATE TABLE marcas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE modelo(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nombre TEXT NOT NULL UNIQUE,
-    marca_id INTEGER,
-    FOREIGN KEY (marca_id) REFERENCES marca(id) ON DELETE CASCADE
-)
+CREATE TABLE modelos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    marca_id INT,
+    FOREIGN KEY (marca_id) REFERENCES marca(id)
+);
+
+CREATE TABLE centros (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    provincia VARCHAR(120) NOT NULL,
+    direccion VARCHAR(120) NOT NULL 
+);
 
 CREATE TABLE vehiculos (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    marca_id  INTEGER,
-    modelo_id INTEGER,
-    precio INTEGER NOT NULL,
-    kilometros_recorridos INTEGER NOT NULL,
-    FOREIGN KEY (marca_id) REFERENCES marca(id) ON DELETE CASCADE,
-    FOREIGN KEY (modelo_id) REFERENCES modelo(id) ON DELETE CASCADE
-)   
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    marca_id  INT,
+    centro_id INT,
+    precio INT NOT NULL,
+    kilometros_recorridos INT NOT NULL,
+    FOREIGN KEY (marca_id) REFERENCES marca(id),
+    FOREIGN KEY (centro_id) REFERENCES centro(id)
+ 
+);  
 
-CREATE TABLE centro (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    provincia TEXT NOT NULL,
-    calle TEXT NOT NULL,
-    vehiculos_id INTEGER,
-    FOREIGN KEY (vehiculos_id) REFERENCES vehiculos(id) ON DELETE CASCADE
+INSERT INTO marcas (nombre) VALUES
+("Toyota"),
+("Ford"),
+("Volkswagen"),
+("BMW"),
+("Mercedes");
 
-
-)
+INSERT INTO modelos (nombre, marca_id) VALUES
+("Corolla", (SELECT WHERE id FROM marca LIKE = "Toyota")),
+("Golf", (SELECT WHERE id FROM marca LIKE = "Toyota")),

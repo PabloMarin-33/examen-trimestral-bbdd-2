0724f26b1fef0ee62d83d98d792689ec1500284a
Palomo-33 <pablomarinortega37@gmail.com>
2026-03-19 10:38:34 +0100

    3º version del schema con las tablas ya creadas tratando de uniendolas

--- mysql-entorno/schema.sql
+++ mysql-entorno/schema.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS tienda_coches;
+USE tienda_coches
+
 CREATE TABLE marca(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nombre TEXT NOT NULL UNIQUE
@@ -5,21 +8,27 @@ CREATE TABLE marca(
 
 CREATE TABLE modelo(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nombre TEXT NOT NULL UNIQUE
+    nombre TEXT NOT NULL UNIQUE,
+    marca_id INTEGER,
+    FOREIGN KEY (marca_id) REFERENCES marca(id) ON DELETE CASCADE
 )
 
 CREATE TABLE vehiculos (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    marca  TEXT NOT NULL,
-    modelo TEXT NOT NULL,
+    marca_id  INTEGER,
+    modelo_id INTEGER,
     precio INTEGER NOT NULL,
-    kilometros_recorridos INTEGER NOT NULL
-)
-     
+    kilometros_recorridos INTEGER NOT NULL,
+    FOREIGN KEY (marca_id) REFERENCES marca(id) ON DELETE CASCADE,
+    FOREIGN KEY (modelo_id) REFERENCES modelo(id) ON DELETE CASCADE
+)   
 
 CREATE TABLE centro (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     provincia TEXT NOT NULL,
-    calle TEXT NOT NULL
+    calle TEXT NOT NULL,
+    vehiculos_id INTEGER,
+    FOREIGN KEY (vehiculos_id) REFERENCES vehiculos(id) ON DELETE CASCADE
+
 
 )
